2d0c7158b410a689083ce09dfae30bb59ff4108a
pepefcardoso <pppfcardoso@gmail.com>
2024-01-26 12:37:04 -0300

    fix: fixing model

--- lib/models/blog_post_category.dart
+++ lib/models/blog_post_category.dart
@@ -1,16 +1,13 @@
 class BlogPostCategory {
   final int? id;
   final String? name;
-  final String? image;
 
   BlogPostCategory({
     this.id,
     this.name,
-    this.image,
   });
 
   BlogPostCategory.fromJson(Map<String, dynamic> json)
       : id = json['id'],
-        name = json['name'],
-        image = json['image'];
+        name = json['name'];
 }
